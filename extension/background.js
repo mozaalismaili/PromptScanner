e068bd503cb3691240e1b864c89ee9d26d996be2
Moza Amur <mozaamur1602@gmail.com>
2026-04-27 13:23:26 +0400

    final version-err

--- extension/background.js
+++ extension/background.js
@@ -63,9 +63,8 @@ async function handleScan(text, hostname, tabId) {
       }
     }
 
-    //await chrome.action.openPopup();
-    console.log("Status:", response.status);
-    console.log("Error:", err);
+    await chrome.action.openPopup();
+
 
   } catch (err) {
     console.error("PromptScanner scan error:", err);
